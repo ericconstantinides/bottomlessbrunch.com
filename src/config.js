3681556279626fbd6abde1e056a9f6662df3fce2
Eric Constantinides <eric@ericconstantinides.com>
2017-09-19 22:59:43 -0700

    Remove cruft from config

--- src/config.js
+++ src/config.js
@@ -1,20 +1,3 @@
-export const regions = {
-  SanJoseCA: {
-    zoom: 11,
-    position: {
-      lat: 37.318835055903456,
-      lng: -121.9353463464844
-    }
-  },
-  SanFranciscoCA: {
-    zoom: 13,
-    position: {
-      lat: 37.7737683,
-      lng: -122.4469884
-    }
-  }
-}
-
 export const googleGa = 'UA-21524856-1'
 export const GOOGLE_API_KEY = 'AIzaSyDsJWQDeqiFe-Chw1tBWfitg_7rGSWmHTU'
 // How often should I reFetch the Google API in minutes
