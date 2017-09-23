184b20702af809ed7d5b82ca391f06e46847a0d2
Eric Constantinides <eric@ericconstantinides.com>
2017-09-23 00:53:22 -0700

    Add parsePath helper function

--- src/lib/myHelpers.js
+++ src/lib/myHelpers.js
@@ -21,3 +21,18 @@ export function reduceVenuesByRegion (venues, regionId) {
 export function roundHalf (num) {
   return Math.round(num * 2) / 2
 }
+
+/**
+ * Parses a pathName and returns it as an array
+ * @param {String} fullPath
+ * @return {Array}
+ */
+export function parsePath (fullPath) {
+  const trimFront = fullPath.substring(0, 1) === '/'
+    ? fullPath.substring(1)
+    : fullPath
+  const trimmed = trimFront.substring((trimFront.length - 1), 1) === '/'
+    ? trimFront.substring(1)
+    : trimFront
+  return trimmed.split('/')
+}
