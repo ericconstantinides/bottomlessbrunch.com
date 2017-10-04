f9a5b3707541359bd0b598a69b1e67b5281aacef
Eric Constantinides <eric@ericconstantinides.com>
2017-10-04 03:06:37 -0700

    Tiny style updates

--- src/components/admin/venues/ListVenues.js
+++ src/components/admin/venues/ListVenues.js
@@ -9,21 +9,24 @@ const AdminVenue = ({ _id, name, lat, lng, zoom, handleDelete }) => {
   return (
     <div className='AdminVenue'>
       <hr />
-      <h2>{name}</h2>
-      <p>lat: {lat}</p>
-      <p>lng: {lng}</p>
-      <Link
-        to={`/admin/venues/${_id}/edit`}
-        className='btn btn-sm btn-primary'
-      >
-        Edit
-      </Link>
-      <button
-        onClick={handleDelete(_id, name)}
-        className='btn btn-sm btn-danger'
-      >
-        Delete
-      </button>
+      <div className='AdminVenue__inner'>
+        <Link
+          to={`/admin/venues/${_id}/edit`}
+          className='btn btn-sm btn-primary'
+        >
+          Edit
+        </Link>
+        <button
+          type='button'
+          onClick={handleDelete(_id, name)}
+          className='btn btn-sm btn-danger'
+        >
+          Delete
+        </button>
+        <h2>{name}</h2>
+        {/* <p>lat: {lat}</p>
+        <p>lng: {lng}</p> */}
+      </div>
     </div>
   )
 }
@@ -36,8 +39,8 @@ class ListVenues extends Component {
   }
   render () {
     return (
-      <div className='AdminVenues container'>
-        <Link to='/admin/venues/add' className='btn btn-sm btn-primary'>
+      <div className='AdminVenues site-container'>
+        <Link to='/admin/venues/add' className='btn btn-sm btn-success'>
           Add Venue
         </Link>
         {_.map(this.props.venues, region => (
