8b7b91e99585b30e651acba4cf862312a1a5b9ad
Eric Constantinides <eric@ericconstantinides.com>
2017-10-24 17:11:50 -0700

    Make venueTeaser efficiently render

--- src/components/MapPage/VenueTeaser.js
+++ src/components/MapPage/VenueTeaser.js
@@ -1,76 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { compileDays } from '../../lib/myHelpers'
+let rendered = 1
 
-const VenueTeaser = ({
-  hoveredVenue,
-  venue,
-  regionSlug,
-  regionName,
-  handleMouseOver,
-  handleMouseLeave,
-  toggleMarkerClick,
-  altClass
-}) => {
-  const hovered = venue._id === hoveredVenue ? 'is-hovered' : 'not-hovered'
+class VenueTeaser extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isActive: false
+    }
+  }
+  componentWillReceiveProps (nextProps) {
+    // set the isActve bool based on if this venue is the hovered venue:
+    this.setState({isActive: nextProps.venue._id === nextProps.hoveredVenue})
+  }
+  shouldComponentUpdate (nextProps, nextState) {
+    // return TRUE if there's a change in the isActive bool
+    return nextState.isActive !== this.state.isActive
+  }
+  render () {
+    const {
+      venue,
+      regionSlug,
+      regionName,
+      handleMouseOver,
+      handleMouseLeave,
+      toggleMarkerClick,
+      altClass
+    } = this.props
+    console.log(`VenueTeaser render() #${rendered++}: ${venue.name}`)
+    const hovered = this.state.isActive ? 'is-hovered' : 'not-hovered'
 
-  const renderedImage = venue.gData && venue.gData.images
-    ? <div
-      className={`VenueTeaser__image-container ${altClass}__image-container`}
-      style={{ backgroundImage: `url(${venue.gData.images.large[0].url})` }}
-      />
-    : ''
-  const funTimes = compileDays(venue.funTimes, 'Bottomless Brunch', venue.name)
-  return (
-    <article
-      className={`VenueTeaser ${altClass} ${hovered}`}
-      onMouseEnter={handleMouseOver(venue)}
-      onMouseLeave={handleMouseLeave(venue)}
-      onClick={toggleMarkerClick(venue)}
-    >
-      {altClass === 'MapItem' &&
-        <div
-          className={`VenueTeaser__marker-container ${altClass}__marker-container`}
-        >
-          <span className={`VenueTeaser__marker ${altClass}__marker`} />
-        </div>
-      }
-      {/* THE VENUETEASER__INNER is where the real link should be... */}
-      <Link
-        regionName={regionName}
-        venue={venue}
-        to={`/${regionSlug}/${venue.slug}`}
+    const renderedImage = venue.gData && venue.gData.images
+      ? <div
+        className={`VenueTeaser__image-container ${altClass}__image-container`}
+        style={{ backgroundImage: `url(${venue.gData.images.large[0].url})` }}
+        />
+      : ''
+    const funTimes = compileDays(venue.funTimes, 'Bottomless Brunch', venue.name)
+    return (
+      <article
+        className={`VenueTeaser ${altClass} ${hovered}`}
+        onMouseEnter={handleMouseOver(venue)}
+        onMouseLeave={handleMouseLeave(venue)}
+        onClick={toggleMarkerClick(venue)}
       >
-        <div className={`VenueTeaser__inner ${altClass}__inner`}>
-          {renderedImage}
-          <div className={`VenueTeaser__content ${altClass}__content`}>
-            <h3 className={`VenueTeaser__title ${altClass}__title`}>
-              {venue.name}
-            </h3>
-            {venue.address &&
-              <p className={`VenueTeaser__p ${altClass}__p`}>
-                {venue.address.street}, {venue.address.city}
-              </p>}
-            {funTimes &&
-              <div className={`VenueTeaser__funtimes ${altClass}__funtimes`}>
-                <h4 className={`VenueTeaser__sub-title ${altClass}__sub-title`}>
-                  Go Bottomless
-                </h4>
-                {funTimes.map((fun, i) => (
-                  <p key={i} className={`VenueTeaser__p ${altClass}__p`}>
-                    <strong>{fun.day}</strong> {fun.startTime} - {fun.endTime}
-                  </p>
-                ))}
-              </div>}
-            <button className={`VenueTeaser__more-info ${altClass}__more-info`}>
-              <span className='text'>More Info</span>
-              <span className='chevron'>»</span>
-            </button>
+        {altClass === 'MapItem' &&
+          <div
+            className={`VenueTeaser__marker-container ${altClass}__marker-container`}
+          >
+            <span className={`VenueTeaser__marker ${altClass}__marker`} />
           </div>
-        </div>
-      </Link>
-    </article>
-  )
+        }
+        {/* THE VENUETEASER__INNER is where the real link should be... */}
+        <Link
+          regionName={regionName}
+          venue={venue}
+          to={`/${regionSlug}/${venue.slug}`}
+        >
+          <div className={`VenueTeaser__inner ${altClass}__inner`}>
+            {renderedImage}
+            <div className={`VenueTeaser__content ${altClass}__content`}>
+              <h3 className={`VenueTeaser__title ${altClass}__title`}>
+                {venue.name}
+              </h3>
+              {venue.address &&
+                <p className={`VenueTeaser__p ${altClass}__p`}>
+                  {venue.address.street}, {venue.address.city}
+                </p>}
+              {funTimes &&
+                <div className={`VenueTeaser__funtimes ${altClass}__funtimes`}>
+                  <h4 className={`VenueTeaser__sub-title ${altClass}__sub-title`}>
+                    Go Bottomless
+                  </h4>
+                  {funTimes.map((fun, i) => (
+                    <p key={i} className={`VenueTeaser__p ${altClass}__p`}>
+                      <strong>{fun.day}</strong> {fun.startTime} - {fun.endTime}
+                    </p>
+                  ))}
+                </div>}
+              <button className={`VenueTeaser__more-info ${altClass}__more-info`}>
+                <span className='text'>More Info</span>
+                <span className='chevron'>»</span>
+              </button>
+            </div>
+          </div>
+        </Link>
+      </article>
+    )
+  }
 }
 
 export default VenueTeaser
