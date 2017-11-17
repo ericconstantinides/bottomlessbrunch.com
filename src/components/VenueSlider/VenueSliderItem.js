0b5f6f108ffedab0207c9091eca07579401036c3
Eric Constantinides <eric@ericconstantinides.com>
2017-11-17 02:59:29 -0800

    Add loading... to venueSliderItem

--- src/components/VenueSlider/VenueSliderItem.js
+++ src/components/VenueSlider/VenueSliderItem.js
@@ -119,164 +119,171 @@ class VenueSliderItem extends Component {
           className='VenueSliderItem__inner'
           style={{ backgroundImage: bgStyle }}
         >
-          <div className='VenueSliderItem__content'>
-            <h1 className='VenueSliderItem__title'>{venue.name}</h1>
-            <h2 className='VenueSliderItem__sub-title'>{displayHood}</h2>
-            <div className='VenueSliderItem__ratings-and-hours'>
-              <div className='VenueSliderItem__ratings'>
-                {venue.googlePlacesData &&
-                  venue.googlePlacesData.rating &&
-                  <div className='VenueSliderItem__ratings-item'>
-                    <h3 className='VenueSliderItem__ratings-title'>Google</h3>
-                    <Star
-                      className='VenueSliderItem__ratings-stars'
-                      size={15}
-                      value={roundHalf(venue.googlePlacesData.rating)}
-                      edit={false}
-                    />
-                  </div>}
-                {venue.yData &&
-                  venue.yData.rating &&
-                  <div className='VenueSliderItem__ratings-item'>
-                    <h3 className='VenueSliderItem__ratings-title'>Yelp</h3>
-                    <Star
-                      className='VenueSliderItem__ratings-stars'
-                      size={15}
-                      value={roundHalf(venue.yData.rating)}
-                      edit={false}
-                    />
-                  </div>}
-              </div>
-              <div className='VenueSliderItem__top-meta'>
-                <div className='VenueSliderItem__address'>
-                  <h3 className='VenueSliderItem__address-title'>Address</h3>
-                  {venue.address &&
-                    <p className='VenueSliderItem__address-p'>
-                      {venue.address.street}<br />
-                      {venue.address.city}<br />
-                      {venue.phone}
+          {venue.name &&
+            <div>
+              <div className='VenueSliderItem__content'>
+                <h1 className='VenueSliderItem__title'>{venue.name}</h1>
+                <h2 className='VenueSliderItem__sub-title'>{displayHood}</h2>
+                <div className='VenueSliderItem__ratings-and-hours'>
+                  <div className='VenueSliderItem__ratings'>
+                    {venue.googlePlacesData &&
+                      venue.googlePlacesData.rating &&
+                      <div className='VenueSliderItem__ratings-item'>
+                        <h3 className='VenueSliderItem__ratings-title'>Google</h3>
+                        <Star
+                          className='VenueSliderItem__ratings-stars'
+                          size={15}
+                          value={roundHalf(venue.googlePlacesData.rating)}
+                          edit={false}
+                        />
+                      </div>}
+                    {venue.yData &&
+                      venue.yData.rating &&
+                      <div className='VenueSliderItem__ratings-item'>
+                        <h3 className='VenueSliderItem__ratings-title'>Yelp</h3>
+                        <Star
+                          className='VenueSliderItem__ratings-stars'
+                          size={15}
+                          value={roundHalf(venue.yData.rating)}
+                          edit={false}
+                        />
+                      </div>}
+                  </div>
+                  <div className='VenueSliderItem__top-meta'>
+                    <div className='VenueSliderItem__address'>
+                      <h3 className='VenueSliderItem__address-title'>Address</h3>
+                      {venue.address &&
+                        <p className='VenueSliderItem__address-p'>
+                          {venue.address.street}<br />
+                          {venue.address.city}<br />
+                          {venue.phone}
+                        </p>
+                      }
+                    </div>
+                    <div className='VenueSliderItem__hours'>
+                      <h3 className='VenueSliderItem__hours-title'>
+                        Regular Hours
+                      </h3>
+                      {hours &&
+                        hours.map((item, i) => (
+                          <p key={i} className='VenueSliderItem__hours-p'>
+                            <strong>{item.weekday}:</strong> {item.time}
+                          </p>
+                        ))}
+                    </div>
+                  </div>
+                </div>
+                <div className='VenueSliderItem__middle'>
+                  <div className='VenueSliderItem__middle-left'>
+                    {venue.yMeta && venue.yMeta.outdoorSeating &&
+                      <div className='VenueSliderItem__middle-meta'>
+                        <h4 className='VenueSliderItem__middle-meta-title'>
+                          Outside Seating
+                        </h4>
+                        <p className='VenueSliderItem__middle-meta-p'>
+                          {venue.yMeta.outdoorSeating ? 'Yes' : 'No'}
+                        </p>
+                      </div>}
+                    {venue.yMeta && venue.yMeta.takesReservations &&
+                      <div className='VenueSliderItem__middle-meta'>
+                        <h4 className='VenueSliderItem__middle-meta-title'>
+                          Takes Reservations
+                        </h4>
+                        <p className='VenueSliderItem__middle-meta-p'>
+                          {venue.yMeta.takesReservations ? 'Yes' : 'No'}
+                        </p>
+                      </div>}
+                    {alcohol !== false &&
+                      <div className='VenueSliderItem__middle-meta'>
+                        <h4 className='VenueSliderItem__middle-meta-title'>
+                          Alcohol Served
+                        </h4>
+                        <p className='VenueSliderItem__middle-meta-p'>
+                          {alcohol}
+                        </p>
+                      </div>}
+                    {ambience !== false &&
+                      <div className='VenueSliderItem__middle-meta'>
+                        <h4 className='VenueSliderItem__middle-meta-title'>
+                          The Scene
+                        </h4>
+                        <p className='VenueSliderItem__middle-meta-p'>
+                          {ambience}
+                        </p>
+                      </div>}
+                  </div>
+                  {(funTimes || (venue.funItems && venue.funItems.length)) &&
+                    <div className='VenueSliderItem__middle-center'>
+                      {funTimes &&
+                        <div className='VenueSliderItem__middle-center-top'>
+                          <h3 className='VenueSliderItem__middle-title'>
+                            Go Bottomless
+                          </h3>
+                          {funTimes.map((fun, i) => (
+                            <div key={i} className='VenueSliderItem__duo'>
+                              <p className='VenueSliderItem__duo-left'>{fun.day}</p>
+                              <p className='VenueSliderItem__duo-right'>
+                                {fun.startTime} - {fun.endTime}
+                              </p>
+                            </div>
+                          ))}
+                        </div>}
+                      {venue.funItems.length > 0 &&
+                        <div className='VenueSliderItem__middle-center-bottom'>
+                          <h3 className='VenueSliderItem__middle-title'>
+                            Bottomless Deals
+                          </h3>
+                          {venue.funItems.map((item, i) => (
+                            <div key={i} className='VenueSliderItem__duo'>
+                              <p className='VenueSliderItem__duo-left'>
+                                ${item.price}
+                              </p>
+                              <p className='VenueSliderItem__duo-right'>
+                                {item.name}
+                              </p>
+                            </div>
+                          ))}
+                        </div>}
+                    </div>}
+                  <div className='VenueSliderItem__middle-right'>
+                    <h3 className='VenueSliderItem__middle-title'>
+                      Share your brunch plans!
+                    </h3>
+                    <p className='u-mb-0_5'>
+                      Tell your girlfriends,<br />
+                      tell your boyfriends!
                     </p>
-                  }
+                    <FacebookShareButton
+                      url={`${SITE_DOMAIN}${this.props.history.location.pathname}`}
+                      hashtag='#bottomlessbrunch'
+                    >
+                      <FacebookIcon
+                        iconBgStyle={{ fill: 'transparent' }}
+                        size={40}
+                      />
+                      <span>Share on Facebook</span>
+                    </FacebookShareButton>
+                    <TwitterShareButton
+                      url={`${SITE_DOMAIN}${this.props.history.location.pathname}`}
+                      hashtag='#bottomlessbrunch'
+                    >
+                      <TwitterIcon
+                        iconBgStyle={{ fill: 'transparent' }}
+                        size={40}
+                      />
+                      <span>Share on Twitter</span>
+                    </TwitterShareButton>
+                  </div>
                 </div>
-                <div className='VenueSliderItem__hours'>
-                  <h3 className='VenueSliderItem__hours-title'>
-                    Regular Hours
-                  </h3>
-                  {hours &&
-                    hours.map((item, i) => (
-                      <p key={i} className='VenueSliderItem__hours-p'>
-                        <strong>{item.weekday}:</strong> {item.time}
-                      </p>
-                    ))}
-                </div>
+              </div>
+              <div className='VenueSliderItem__image-container'>
+                {photos}
               </div>
             </div>
-            <div className='VenueSliderItem__middle'>
-              <div className='VenueSliderItem__middle-left'>
-                {venue.yMeta && venue.yMeta.outdoorSeating &&
-                  <div className='VenueSliderItem__middle-meta'>
-                    <h4 className='VenueSliderItem__middle-meta-title'>
-                      Outside Seating
-                    </h4>
-                    <p className='VenueSliderItem__middle-meta-p'>
-                      {venue.yMeta.outdoorSeating ? 'Yes' : 'No'}
-                    </p>
-                  </div>}
-                {venue.yMeta && venue.yMeta.takesReservations &&
-                  <div className='VenueSliderItem__middle-meta'>
-                    <h4 className='VenueSliderItem__middle-meta-title'>
-                      Takes Reservations
-                    </h4>
-                    <p className='VenueSliderItem__middle-meta-p'>
-                      {venue.yMeta.takesReservations ? 'Yes' : 'No'}
-                    </p>
-                  </div>}
-                {alcohol !== false &&
-                  <div className='VenueSliderItem__middle-meta'>
-                    <h4 className='VenueSliderItem__middle-meta-title'>
-                      Alcohol Served
-                    </h4>
-                    <p className='VenueSliderItem__middle-meta-p'>
-                      {alcohol}
-                    </p>
-                  </div>}
-                {ambience !== false &&
-                  <div className='VenueSliderItem__middle-meta'>
-                    <h4 className='VenueSliderItem__middle-meta-title'>
-                      The Scene
-                    </h4>
-                    <p className='VenueSliderItem__middle-meta-p'>
-                      {ambience}
-                    </p>
-                  </div>}
-              </div>
-              {(funTimes || (venue.funItems && venue.funItems.length)) &&
-                <div className='VenueSliderItem__middle-center'>
-                  {funTimes &&
-                    <div className='VenueSliderItem__middle-center-top'>
-                      <h3 className='VenueSliderItem__middle-title'>
-                        Go Bottomless
-                      </h3>
-                      {funTimes.map((fun, i) => (
-                        <div key={i} className='VenueSliderItem__duo'>
-                          <p className='VenueSliderItem__duo-left'>{fun.day}</p>
-                          <p className='VenueSliderItem__duo-right'>
-                            {fun.startTime} - {fun.endTime}
-                          </p>
-                        </div>
-                      ))}
-                    </div>}
-                  {venue.funItems.length > 0 &&
-                    <div className='VenueSliderItem__middle-center-bottom'>
-                      <h3 className='VenueSliderItem__middle-title'>
-                        Bottomless Deals
-                      </h3>
-                      {venue.funItems.map((item, i) => (
-                        <div key={i} className='VenueSliderItem__duo'>
-                          <p className='VenueSliderItem__duo-left'>
-                            ${item.price}
-                          </p>
-                          <p className='VenueSliderItem__duo-right'>
-                            {item.name}
-                          </p>
-                        </div>
-                      ))}
-                    </div>}
-                </div>}
-              <div className='VenueSliderItem__middle-right'>
-                <h3 className='VenueSliderItem__middle-title'>
-                  Share your brunch plans!
-                </h3>
-                <p className='u-mb-0_5'>
-                  Tell your girlfriends,<br />
-                  tell your boyfriends!
-                </p>
-                <FacebookShareButton
-                  url={`${SITE_DOMAIN}${this.props.history.location.pathname}`}
-                  hashtag='#bottomlessbrunch'
-                >
-                  <FacebookIcon
-                    iconBgStyle={{ fill: 'transparent' }}
-                    size={40}
-                  />
-                  <span>Share on Facebook</span>
-                </FacebookShareButton>
-                <TwitterShareButton
-                  url={`${SITE_DOMAIN}${this.props.history.location.pathname}`}
-                  hashtag='#bottomlessbrunch'
-                >
-                  <TwitterIcon
-                    iconBgStyle={{ fill: 'transparent' }}
-                    size={40}
-                  />
-                  <span>Share on Twitter</span>
-                </TwitterShareButton>
-              </div>
-            </div>
-          </div>
-          <div className='VenueSliderItem__image-container'>
-            {photos}
-          </div>
+          }
+          {!venue.name &&
+            <h1>Loading...</h1>
+          }
         </div>
       </article>
     )
