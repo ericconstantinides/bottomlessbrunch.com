5ad7fee8825d5e72bf8f119f4f7f10dccb483d42
Eric Constantinides <eric@ericconstantinides.com>
2018-01-03 22:31:31 -0800

    More error control

--- src/actions/filterActions.js
+++ src/actions/filterActions.js
@@ -39,35 +39,39 @@ export const constructFilters = (venues, visibleVenues) => {
   const fltrs = _.cloneDeep(initialState)
   visibleVenues.forEach(({ _id }) => {
     const { normalizedTimes, normalizedDrinks } = venues[_id]
-    normalizedTimes.forEach(day => {
-      fltrs.timeMin = fltrs.timeMin === -1 || day.startTime < fltrs.timeMin
-        ? day.startTime
-        : fltrs.timeMin
-      fltrs.timeMax = fltrs.timeMax === -1 || day.endTime > fltrs.timeMax
-        ? day.endTime
-        : fltrs.timeMax
-      fltrs.dayMin = fltrs.dayMin === -1 || day.day < fltrs.dayMin
-        ? day.day
-        : fltrs.dayMin
-      fltrs.dayMax = fltrs.dayMin === -1 || day.day > fltrs.dayMax
-        ? day.day
-        : fltrs.dayMax
-    })
-    normalizedDrinks.forEach(drinkObj => {
-      fltrs.priceMax = fltrs.priceMax === -1 || drinkObj.price > fltrs.priceMax
-        ? drinkObj.price
-        : fltrs.priceMax
-      fltrs.includeDrinkWithMealPrices = {
-        disabled: !(fltrs.includeDrinkWithMealPrices.checked ||
-          drinkObj.priceIncludesFood),
-        checked: fltrs.includeDrinkWithMealPrices.checked ||
-          drinkObj.priceIncludesFood
-      }
-      fltrs.drinks[drinkObj.drink] = {
-        ...fltrs.drinks[drinkObj.drink],
-        disabled: false
-      }
-    })
+    if (normalizedTimes && normalizedTimes.length) {
+      normalizedTimes.forEach(day => {
+        fltrs.timeMin = fltrs.timeMin === -1 || day.startTime < fltrs.timeMin
+          ? day.startTime
+          : fltrs.timeMin
+        fltrs.timeMax = fltrs.timeMax === -1 || day.endTime > fltrs.timeMax
+          ? day.endTime
+          : fltrs.timeMax
+        fltrs.dayMin = fltrs.dayMin === -1 || day.day < fltrs.dayMin
+          ? day.day
+          : fltrs.dayMin
+        fltrs.dayMax = fltrs.dayMin === -1 || day.day > fltrs.dayMax
+          ? day.day
+          : fltrs.dayMax
+      })
+    }
+    if (normalizedDrinks && normalizedDrinks.length) {
+      normalizedDrinks.forEach(drinkObj => {
+        fltrs.priceMax = fltrs.priceMax === -1 || drinkObj.price > fltrs.priceMax
+          ? drinkObj.price
+          : fltrs.priceMax
+        fltrs.includeDrinkWithMealPrices = {
+          disabled: !(fltrs.includeDrinkWithMealPrices.checked ||
+            drinkObj.priceIncludesFood),
+          checked: fltrs.includeDrinkWithMealPrices.checked ||
+            drinkObj.priceIncludesFood
+        }
+        fltrs.drinks[drinkObj.drink] = {
+          ...fltrs.drinks[drinkObj.drink],
+          disabled: false
+        }
+      })
+    }
   })
   fltrs.timeMin = Math.floor(fltrs.timeMin)
   fltrs.timeStart = fltrs.timeMin
